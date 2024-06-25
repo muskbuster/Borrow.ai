80b9d1c216739937042770aca88ccc296336add3
muskbuster <sudeep@inco.network>
2024-06-25 19:02:50 +0000

    skeleton

---
+++ Backend/Contract_Scripts/Aave_PoolDeets.js
@@ -0,0 +1,3 @@
+const ethers = require('ethers');
+const { AvaxMainet, ArbitrumMainet } = require('../Configs/NetworkConfig');
+const {AAVE_POOL_ABI_AVAX,AAVE_UI_POOL_AVAX,AAVE_DATAPROV_ABI_AVAX}= require('../Configs/ABI');
