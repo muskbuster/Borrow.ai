0451a906563b66802eba7b446e457823fe736bbb
muskbuster <sudeep@inco.network>
2024-06-28 17:47:53 +0000

    contractCall

--- Backend/Configs/NetworkConfig.js
+++ Backend/Configs/NetworkConfig.js
@@ -2,8 +2,10 @@ const { ethers } = require("ethers");
 
 const AvaxMainet= new ethers.JsonRpcProvider("https://api.avax.network/ext/bc/C/rpc");
 const ArbitrumMainet= new ethers.JsonRpcProvider("https://arb1.arbitrum.io/rpc");
+const AvaxTestnet= new ethers.JsonRpcProvider("wss://avalanche-fuji-c-chain-rpc.publicnode.com");
 
 module.exports = {
     AvaxMainet,
     ArbitrumMainet,
+    AvaxTestnet
     };
